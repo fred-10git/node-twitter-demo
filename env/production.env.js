6c262fd70f6df3d3d8a68e38e52f2d02a5f95822
fred-10git.fr <contact@10git.fr>
2022-05-01 19:05:14 +0200

    add prod cert

--- env/production.env.js
+++ env/production.env.js
@@ -1,10 +1,10 @@
-const path = require("path");
-
 module.exports = {
   databaseUrl: "mongodb+srv://10git:123@cluster0.s465p.mongodb.net/twitter?retryWrites=true&w=majority",
   httpPort: 80,
   httpsPort: 443,
-  cert: path.join(__dirname, ""),
-  key: path.join(__dirname, ""),
-  passPhrase: "",
+  cert: "/etc/letsencrypt/live/twitter.10git.fr/fullchain.pem",
+  key: "/etc/letsencrypt/live/twitter.10git.fr/privkey.pem",
 };
+
+git commit -m "add prod cert"
+git push origin main 
